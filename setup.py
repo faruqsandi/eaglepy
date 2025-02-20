73f2aa9f95dcc07f3229f7d422807d2e802a6102
Faruq Sandi <faruqsandi@gmail.com>
2025-02-20 23:39:56 +0700

    Update setup

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="eaglepy",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
-    description="A simple example Python package",
+    description="Eagle API Wrapper for Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Your Name",
-    author_email="your.email@example.com",
-    url="https://github.com/yourusername/eaglepy",
+    author_email="faruqsandi@gmail.com",
+    url="https://github.com/faruqsandi/eaglepy",
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=["requests"],
     python_requires='>=3.6',
 )
